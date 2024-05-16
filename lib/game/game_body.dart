0beb4a1bff0f79615b149883f9032f80b12cb9d1
sssun <98_0509@naver.com>
2024-05-16 23:42:01 +0900

    화면 구성

---
+++ lib/game/game_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameBody extends StatefulWidget {
+  const GameBody({super.key});
+
+  @override
+  State<GameBody> createState() => _GameBodyState();
+}
+
+class _GameBodyState extends State<GameBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
